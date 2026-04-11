4c9ffbd44ada64d91cbba2b92f0c88aa99442e54
Pasha <Pasha@LAPTOP-5Q537UU6>
2026-04-11 15:11:55 +0300

    Телеграмм бот

---
+++ BotTelegram/DTOs/Responses/AdminRegistrationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTelegram.DTOs.Responses
+{
+    public class AdminRegistrationResponse
+    {
+        public string Message { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ BotTelegram/DTOs/Responses/StatsTopNegativeUsersResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotTelegram.DTOs.Responses
+{
+    public class StatsTopNegativeUsersResponse
+    {
+        public long UserIdTg { get; set; }
+        public int TotalMes { get; set; }
+        public int NegativeMes { get; set; }
+        public int PositiveMes { get; set; }
+        public double NegativePercent { get; set; }
+        public double PositivePercent { get; set; }
+    }
+}
